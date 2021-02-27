de1eac9080ce1580f9db87ab68e90472f5955238
maxluchs <>
2021-02-27 16:47:23 +0100

    join ventures Inc.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ use std::error::Error;
 
 mod schema;
 
-#[derive(Queryable, Debug)]
+#[derive(Queryable, Debug, Identifiable, AsChangeset)]
+#[table_name = "users"]
 struct User {
     id: i32,
     name: String,
@@ -36,13 +37,22 @@ struct Participation {
 #[derive(Debug, Queryable)]
 struct UserCourse {
     user: User,
-    participation: ParticipationCourse
+    participation: ParticipationCourse,
 }
 
 #[derive(Debug, Queryable)]
 struct ParticipationCourse {
     participation: Participation,
-    course: Course
+    course: Course,
+}
+
+use schema::users;
+
+#[derive(Debug, Insertable)]
+#[table_name = "users"]
+pub struct NewUser {
+    name: String,
+    gender: i32,
 }
 
 fn main() -> std::result::Result<(), Box<dyn Error>> {
@@ -73,13 +83,12 @@ fn main() -> std::result::Result<(), Box<dyn Error>> {
         .filter(schema::users::name.eq("Sara")).load::<(User, (Participation, Course))>(con)?;
     info!("saras_courses {:?}", saras_courses_tuples);
 
-
     let saras_courses_struct = schema::users::table
         .inner_join(
-        schema::participations::table.inner_join(
-            schema::courses::table)
-    )
-      .filter(schema::users::name.eq("Sara")).load::<UserCourse>(con)?;
+            schema::participations::table.inner_join(
+                schema::courses::table)
+        )
+        .filter(schema::users::name.eq("Sara")).load::<UserCourse>(con)?;
     info!("saras_courses {:?}", saras_courses_struct);
 
     let saras_courses_titles = schema::users::table
@@ -92,5 +101,28 @@ fn main() -> std::result::Result<(), Box<dyn Error>> {
 
     let courses = schema::courses::table.get_results::<Course>(con)?;
     info!("courses {:?}", courses);
+
+    let new_user1 = NewUser { name: "Jonas 123".to_owned(), gender: 1 };
+    let new_user1_inserted = diesel::insert_into(schema::users::table).values(&new_user1).get_result::<User>(con)?;
+    info!("New user: {:?} inserted: {:?}", new_user1, new_user1_inserted);
+
+    let user1_update = User { name: "Mr wurst".to_owned(), ..new_user1_inserted };
+    info!("Updating user : {:?}", user1_update);
+    let user1_updated_result = diesel::update(&user1_update).set(&user1_update).get_result::<User>(con)?;
+    info!("updated user : {:?}", user1_updated_result);
+
+    let new_user = NewUser {
+        gender: 0,
+        name: "Mr twister".to_owned(),
+    };
+    let new_user_inserted = diesel::insert_into(schema::users::table).values(&new_user).get_result::<User>(con)?;
+    info!("New user {:?} inserted {:?}", &new_user, &new_user_inserted);
+
+    let deleted_user = diesel::delete(&new_user_inserted).get_result::<User>(con);
+    info!("deleted user : {:?}", deleted_user);
+
+    let inserted_user = diesel::insert_into(schema::users::table).values(NewUser { gender: 0, name: "Esel".to_owned() }).get_result::<User>(con)?;
+    info!("inserted : {:?}", inserted_user);
+
     return Ok(());
 }
